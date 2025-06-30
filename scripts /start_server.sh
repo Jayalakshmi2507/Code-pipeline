66246faeb76309d7e24affad23ddae69f25eb42c
Jaya <75557993+Jayalakshmi2507@users.noreply.github.com>
2025-06-30 22:35:54 +0530

    Update start_server.sh

--- scripts /start_server.sh
+++ scripts /start_server.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Navigate to the directory where the JAR file is located
-cd /home/ubuntu/
-
-# Start the Java application
-java -jar app.jar > /var/log/app.log 2>&1 &
+systemctl start httpd
+systemctl enable httpd
